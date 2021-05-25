efd77c1f53de1aebd71d77730a061119eaf960db
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2021-05-25 14:07:08 +0900

    Create project

---
+++ mysettings.py
@@ -0,0 +1,13 @@
+
+SECRET_KEY = 'django-insecure-@sp#$p0_r_rj!co8sd=uu963qw$_$*@u#(5n6#!diy!4tfs#0&'
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crud1',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
